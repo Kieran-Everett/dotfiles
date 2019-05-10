24dde571082363ca3b90cfba5629a3a22e581a54
Kieran Everett <kieraneverett03@gmail.com>
2019-05-10 21:22:19 +0100

    Uploaded .config

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,122 @@
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Birocratic - Chillhop Essentials - Fall 2018 - 01 Belly Breathing.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Flitz&Suppe - Chillhop Essentials - Fall 2018 - 02 Funkaholic.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/L'indВcis x sad toЛ - Chillhop Essentials - Fall 2018 - 03 Dog Days.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Tane - Chillhop Essentials - Fall 2018 - 04 Yes Please.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/B-Side - Chillhop Essentials - Fall 2018 - 05 Pen Anubis.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Toonorth - Chillhop Essentials - Fall 2018 - 06 To The Moon And Back.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/plusma - Chillhop Essentials - Fall 2018 - 07 mochus.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Loupo - Chillhop Essentials - Fall 2018 - 08 1993.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/middle school & Taro - Chillhop Essentials - Fall 2018 - 09 At This Point (feat. Mitchel Forman).flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Psalm Trees - Chillhop Essentials - Fall 2018 - 10 Babyyy.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Kupla - Chillhop Essentials - Fall 2018 - 11 Ruska.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/illiterate - Chillhop Essentials - Fall 2018 - 12 Bastia.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/delayde - Chillhop Essentials - Fall 2018 - 13 To Breathe.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/j'san x nymano - Chillhop Essentials - Fall 2018 - 14 autumn breeze.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/fantompower - Chillhop Essentials - Fall 2018 - 15 in a new space.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Stan Forebee - Chillhop Essentials - Fall 2018 - 16 Drawing Dunes.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Matt Quentin - Chillhop Essentials - Fall 2018 - 17 Just a Moment.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Chris Mazuera - Chillhop Essentials - Fall 2018 - 18 Equinox.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/idealism - Chillhop Essentials - Fall 2018 - 19 illusions.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Aso - Chillhop Essentials - Fall 2018 - 20 first date.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/sleepy fish x Philanthrope x mommy - Chillhop Essentials - Fall 2018 - 21 morning rises.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/masked man - Chillhop Essentials - Fall 2018 - 22 grandiose soul.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/invention_ - Chillhop Essentials - Fall 2018 - 23 ebb-flo.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Through & Through - Chillhop Essentials - Fall 2018 - 24 Equanimity.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/AJMW - Chillhop Essentials - Fall 2018 - 25 Nights In West.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Fall 2018/Amonos - Chillhop Essentials - Fall 2018 - 26 Montrose.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Cap Kendricks - Chillhop Essentials - Spring 2018 - 01 The One.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/j'san. - Chillhop Essentials - Spring 2018 - 02 good morning sunshine.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Juan RIOS - Chillhop Essentials - Spring 2018 - 03 Azahar.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/HM Surf - Chillhop Essentials - Spring 2018 - 04 Swix.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Ruck P - Chillhop Essentials - Spring 2018 - 05 Spring in La Coruдa.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Stan Forebee & Kyle McEvoy - Chillhop Essentials - Spring 2018 - 06 Kensington.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Birocratic - Chillhop Essentials - Spring 2018 - 07 Handsome People.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/OTESLA - Chillhop Essentials - Spring 2018 - 08 Carbon.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Monma - Chillhop Essentials - Spring 2018 - 09 ManaTree.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Leavv - Chillhop Essentials - Spring 2018 - 10 Tomorrow.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Cloudchord - Chillhop Essentials - Spring 2018 - 11 H'okay (ft. Soul Food Horns).flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/FloFilz - Chillhop Essentials - Spring 2018 - 12 Blue Orchard.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/mommy & snowglobe - Chillhop Essentials - Spring 2018 - 13 taipei rosebuds.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/illiterate - Chillhop Essentials - Spring 2018 - 14 Moonshine.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Aso & Middle School - Chillhop Essentials - Spring 2018 - 15 Tomorrow Never Knows.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/City Girl - Chillhop Essentials - Spring 2018 - 16 Eowyn.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Joe Corfield - Chillhop Essentials - Spring 2018 - 17 Wildflower.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Psalm Trees - Chillhop Essentials - Spring 2018 - 18 Wherever You Are.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/plusma - Chillhop Essentials - Spring 2018 - 19 troubadix.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Toonorth - Chillhop Essentials - Spring 2018 - 20 Drop Top.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/Robot Orchestra - Chillhop Essentials - Spring 2018 - 21 thinking of you.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2018/deeB - Chillhop Essentials - Spring 2018 - 22 Morningview.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Pandrezz - Chillhop Essentials - Summer 2018 - 01 Takin' You For a Ride.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/C Y G N - Chillhop Essentials - Summer 2018 - 02 Love is Weakness.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Misha - Chillhop Essentials - Summer 2018 - 03 Bae.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Remulak & Liphe - Chillhop Essentials - Summer 2018 - 04 Wonder.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Matt Quentin - Chillhop Essentials - Summer 2018 - 05 Sky Blue.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/plusma - Chillhop Essentials - Summer 2018 - 06 albatros.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Birocratic - Chillhop Essentials - Summer 2018 - 07 Shakedown.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Sofasound - Chillhop Essentials - Summer 2018 - 08 Love Like a River.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/B-Side x Sixcube - Chillhop Essentials - Summer 2018 - 09 Don't Cry.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/floridomi - Chillhop Essentials - Summer 2018 - 10 Lakeside.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Aso - Chillhop Essentials - Summer 2018 - 11 Sundays.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/j'san. x Cloudchord - Chillhop Essentials - Summer 2018 - 12 First Wonder.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Unda de Sango - Chillhop Essentials - Summer 2018 - 13 The Lime Vibe.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/L'indВcis x Saib - Chillhop Essentials - Summer 2018 - 14 By the Seaside.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/falcxne x Loupo - Chillhop Essentials - Summer 2018 - 15 Prayer.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Stan Forebee - Chillhop Essentials - Summer 2018 - 16 Possums & Popcorn.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/mommy x delayde - Chillhop Essentials - Summer 2018 - 17 flashes of calm.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Eli Way x Emily Muli - Chillhop Essentials - Summer 2018 - 18 Stuck.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Lakey Inspired - Chillhop Essentials - Summer 2018 - 19 Saydee.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Tane - Chillhop Essentials - Summer 2018 - 20 Zen.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/santpoort - Chillhop Essentials - Summer 2018 - 21 rolling down this lazy wave.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/Ian Ewing x Philanthrope - Chillhop Essentials - Summer 2018 - 22 Smart Gurl in a Skort.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Summer 2018/HM surf - Chillhop Essentials - Summer 2018 - 23 Oracle Night.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Philanthrope x Yasper - Chillhop Essentials - Winter 2018 - 01 Slopes.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Melodiesinfonie - Chillhop Essentials - Winter 2018 - 02 Morning Glory.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Blue Wednesday - Chillhop Essentials - Winter 2018 - 03 Middle School (feat. Magnus Klausen).flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Juan RIOS - Chillhop Essentials - Winter 2018 - 04 Petricor.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Toonorth - Chillhop Essentials - Winter 2018 - 05 Citrine.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/BEN BADA BOOM - Chillhop Essentials - Winter 2018 - 06 Mistleflow.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/fantompower - Chillhop Essentials - Winter 2018 - 07 blankets.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Ian Ewing - Chillhop Essentials - Winter 2018 - 08 seltzy.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/mommy - Chillhop Essentials - Winter 2018 - 09 polarbeer.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Nokiaa x Dontcry - Chillhop Essentials - Winter 2018 - 10 Viaduct.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Ruck P - Chillhop Essentials - Winter 2018 - 11 Gratitude.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/TESK - Chillhop Essentials - Winter 2018 - 12 Burn my Mind.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Osvaldo - Chillhop Essentials - Winter 2018 - 13 Embers in the Dark (feat. Garot Michael Conklin).flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Louk x Yasper - Chillhop Essentials - Winter 2018 - 14 Captain Calvin.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Strehlow - Chillhop Essentials - Winter 2018 - 15 Mila Coolness.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Monma - Chillhop Essentials - Winter 2018 - 16 Potion.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Sofasound - Chillhop Essentials - Winter 2018 - 17 With All I Am.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/harris cole - Chillhop Essentials - Winter 2018 - 18 larkspur.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Hanz - Chillhop Essentials - Winter 2018 - 19 Crossroads.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Sleepy Fish - Chillhop Essentials - Winter 2018 - 20 Fall's Echos.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/sad toЛ - Chillhop Essentials - Winter 2018 - 21 Love Affair in Kyoto.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/invention_ - Chillhop Essentials - Winter 2018 - 22 snowluv.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Smoke Trees - Chillhop Essentials - Winter 2018 - 23 Winter Lights.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/a l e x - Chillhop Essentials - Winter 2018 - 24 I need to paint my walls.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/leavv - Chillhop Essentials - Winter 2018 - 25 Candle.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Winter 2018/Aso - Chillhop Essentials - Winter 2018 - 26 Rainy Days.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Blue Wednesday - Chillhop Essentials - Spring 2019 - 01 Murmuration (feat. Shopan).flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Cloudchord x G Mills - Chillhop Essentials - Spring 2019 - 02 Sunlit.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Plusma - Chillhop Essentials - Spring 2019 - 03 Marmots Dance.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/masked man - Chillhop Essentials - Spring 2019 - 04 Ish Happens.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/santpoort - Chillhop Essentials - Spring 2019 - 05 Rosemary Whispers.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Aso - Chillhop Essentials - Spring 2019 - 06 Sunsets.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/oddfish - Chillhop Essentials - Spring 2019 - 07 Hazy....flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Nokiaa - Chillhop Essentials - Spring 2019 - 08 Blending In.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Moods x Philanthrope x Yasper - Chillhop Essentials - Spring 2019 - 09 Bucket List.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/stream_error, H.1 - Chillhop Essentials - Spring 2019 - 10 City Lights.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Eli Way - Chillhop Essentials - Spring 2019 - 11 Daydream.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Flitz&Suppe, Gas-Lab - Chillhop Essentials - Spring 2019 - 12 Slice of Life.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Psalm Trees - Chillhop Essentials - Spring 2019 - 13 Meadows (feat. Guillaume Muschalle).flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/SAINT WKND x No Spirit - Chillhop Essentials - Spring 2019 - 14 Changed My Mind.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/B-Side, Yasper, Philanthrope - Chillhop Essentials - Spring 2019 - 15 Last Snow.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Strehlow x Ian Ewing - Chillhop Essentials - Spring 2019 - 16 Poke Date.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/invention_ - Chillhop Essentials - Spring 2019 - 17 Levitating.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Vhsceral - Chillhop Essentials - Spring 2019 - 18 Alamein.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/weird inside - Chillhop Essentials - Spring 2019 - 19 Sinkerator (feat. Dilip & quickly, quickly).flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Ian Ewing - Chillhop Essentials - Spring 2019 - 20 leaving.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Flovry - Chillhop Essentials - Spring 2019 - 21 Laze.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/Monma x cocabona - Chillhop Essentials - Spring 2019 - 22 Pyra.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/fantompower - Chillhop Essentials - Spring 2019 - 23 Morning Dew.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/sleepy fish - Chillhop Essentials - Spring 2019 - 24 I Wish It Would Never Stop Snowing.flac
+/home/kieran/Music/Chillhop Records - Chillhop Essentials - Spring 2019/harris Cole - Chillhop Essentials - Spring 2019 - 25 Birds.flac
